4eceaed6710d5939cf9e46cd7dd531a470903ec8
Swathi <pottigariswathi@gmail.com>
2019-09-08 11:18:31 -0700

    Used hide and show functions instead of adding a hide class and show class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,16 +15,21 @@ var notAnswered = 0;
 var askedQuestions = [];
 var intervalId;
 
-window.onload=function(){
-    $(".questionWrapper").addClass("hide");
-    $(".answerWrapper").addClass("hide");
+window.onload = function () {
+    $(".questionWrapper").hide();
+    $(".answerWrapper").hide();
+    $(".scoreCard").hide();
 };
 
 //  This function is called when a Start button is clicked
 // Functionality : It displays the question to the user
 $("#start").click(function () {
+    if ($(this).text() === "Sure") {
+        reset();
+    }
     displayQstn();
-    $("#start").attr("class", "hide");
+    $("#start").hide();
+    $("#subTitle").hide();
 });
 
 // This function is called when any of the answer option is clicked
@@ -47,28 +52,22 @@ $(".ans").click(function () {
 // Functionality: This reveals the user if the user has selected a correct option or not
 function revealAnswer(status, answer) {
     $("#status").text(status);
-    $(".answerWrapper").removeClass("hide");
+    $(".answerWrapper").show();
     if (answer !== '') {
         $("#answer").text("The correct answer was:" + answer);
-        // $("#answer").removeClass("hide");
     }
     else {
         $("#answer").addClass("hide");;
     }
-    $(".questionWrapper").addClass("hide");
-    // $("#imageDiv").removeClass("hide");
+    $(".questionWrapper").hide();
 }
 
 //This function is called when a user clicks on start button
 // Functionality: It displays the questions to the user
 function displayQstn() {
-
-    $(".questionWrapper").removeClass("hide");
+    $(".questionWrapper").show();
     randomQstn = generateRandom();
-    // $("#status").addClass("hide");
-    // $("#imageDiv").addClass("hide");
-    // $("#answer").addClass("hide");
-     $(".answerWrapper").addClass("hide");
+    $(".answerWrapper").hide();
     if (!askedQuestions.includes(randomQstn.q)) {
         intervalId = setInterval(decrement, 1000);
         askedQuestions.push(randomQstn.q);
@@ -80,14 +79,12 @@ function displayQstn() {
     else {
         displayQstn();
     }
-
 };
 
 //This function is called to select a random question from an array
 function generateRandom() {
     var random = qstnsArray[Math.floor(Math.random() * qstnsArray.length)];
     return random;
-
 }
 
 // This function is used to decrease the timer
@@ -105,20 +102,33 @@ function decrement() {
 // This function is used to reset the timer after each question
 function resetTimer() {
     timer = 5;
-    // $(".timer").addClass("Hide");
-    // $("#timeRemaining").text('');
     clearInterval(intervalId);
 }
 
 // This function is used to check if the questions are completed
 function checkIfGameOver() {
     if (qstnsArray.length === askedQuestions.length) {
-        // $("#status").text("Game Over");
-        // $("#imageDiv").addClass("hide");
-        // $("#answer").addClass("hide");
-        $(".answerWrapper").addClass("hide");
+        $(".answerWrapper").hide();
+        $(".scoreCard").show();
+        $("#wins").text("Correct Answers" + correct);
+        $("#lost").text("Incorrect Answers" + incorrect);
+        $("#notanswered").text("Unanswers" + notAnswered);
+        $("#start").show();
+        $("#start").text("Sure");
+        $("#subTitle").show();
+        $("#subTitle").text("Do you want to play again");
     }
     else {
         displayQstn();
     }
+}
+
+// This function resets the game if user wants to play again
+function reset() {
+    resetTimer();
+    $(".scoreCard").hide();
+    askedQuestions = [];
+    correct = 0;
+    incorrect = 0;
+    notAnswered = 0;
 }
